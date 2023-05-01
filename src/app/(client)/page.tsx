2317b7e9996f6a76083d17e313ab1e41223cc3a3
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-05-01 21:04:25 +0800

    feat: Remove unused code in landing page

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -1,36 +1,7 @@
-'use client';
-
-import { signIn, signOut, useSession } from 'next-auth/react';
-
 const Home = () => {
-  const { status, data: session } = useSession();
-
-  const userInfo = session?.user;
-
-  if (status === 'loading') {
-    return (
-      <div>
-        <p>Loading ...</p>
-      </div>
-    );
-  }
-
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="mb-4 text-xl text-red-400">Landing Page</h1>
-
-      {userInfo ? (
-        <div className="flex flex-col items-center justify-center gap-4">
-          <p>Signed in as {userInfo?.email || '-'}</p>
-          <button className="bg-amber-300 px-4 py-2" onClick={() => signOut()}>
-            Sign Out
-          </button>
-        </div>
-      ) : (
-        <button className="bg-slate-300 px-4 py-2" onClick={() => signIn()}>
-          SignIn
-        </button>
-      )}
     </div>
   );
 };
