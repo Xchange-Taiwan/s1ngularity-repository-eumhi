00955b8fa5c771b15a543b46eaca9400b98466a9
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-05-13 23:56:32 +0800

    feat: Add account and password signin

--- src/app/(client)/signin/page.tsx
+++ src/app/(client)/signin/page.tsx
@@ -1,8 +1,12 @@
 'use client';
 import { ErrorMessage, Form, Formik, Field } from 'formik';
-import { signIn } from 'next-auth/react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
-const SignInPage = () => {
+const LogInPage = () => {
+  const { data: session } = useSession();
+
+  console.log(session);
+
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -17,37 +21,33 @@ const SignInPage = () => {
             account: '',
             password: '',
           }}
-          onSubmit={async (values, { setSubmitting }) => {
-            await new Promise((r) => setTimeout(r, 500));
+          onSubmit={async (params, { setSubmitting }) => {
+            await signIn('credentials', { callbackUrl: '/signin', ...params });
             setSubmitting(false);
           }}
         >
           <Form>
             <div className="pb-4">
-              <label
-                className="block text-sm font-medium leading-6 text-gray-900"
-                htmlFor="account"
-              >
+              <label className="block text-sm font-medium leading-6 text-gray-900">
                 Account
               </label>
               <Field
                 type="text"
-                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                 name="account"
+                placeholder="Enter your account"
               />
               <ErrorMessage name="account" />
             </div>
             <div className="pb-4">
-              <label
-                className="block text-sm font-medium leading-6 text-gray-900"
-                htmlFor="password"
-              >
+              <label className="block text-sm font-medium leading-6 text-gray-900">
                 Password
               </label>
               <Field
                 type="password"
-                className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                 name="password"
+                placeholder="Enter your password"
               />
               <ErrorMessage name="password" />
             </div>
@@ -80,10 +80,20 @@ const SignInPage = () => {
           >
             Sign in with LinkedIn
           </button>
+
+          <hr />
+
+          <button
+            type="button"
+            className="mb-4 flex w-full justify-center rounded-md bg-rose-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-rose-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={() => signOut()}
+          >
+            Sign out
+          </button>
         </div>
       </div>
     </div>
   );
 };
 
-export default SignInPage;
+export default LogInPage;
