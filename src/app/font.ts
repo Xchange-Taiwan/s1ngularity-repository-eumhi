7b3258a2167b2d2dd643e8718c76e2cd8d280efa
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-06-10 23:46:30 +0800

    feat: add google font setting

---
+++ src/app/font.ts
@@ -0,0 +1,7 @@
+import { Noto_Sans } from 'next/font/google';
+
+export const notoSans = Noto_Sans({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '../styles/global.css';
 
+import { notoSans } from './font';
 import Provider from './provider';
 
 export const metadata = {
@@ -15,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="zh-TW">
+    <html lang="zh-TW" className={notoSans.className}>
       <body>
         <Provider>{children}</Provider>
       </body>
