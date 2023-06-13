858c4fc55041471027bc11e0694671f7a1a3a4fc
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-06-13 22:46:36 +0800

    fix: Remove MUI CSSBasline Setting

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,18 +1,9 @@
 'use client';
-import CssBaseline from '@mui/material/CssBaseline';
-import { StyledEngineProvider } from '@mui/material/styles';
 import { SessionProvider } from 'next-auth/react';
 import { FC, ReactNode } from 'react';
 
 const Provider: FC<{ children: ReactNode }> = ({ children }) => {
-  return (
-    <StyledEngineProvider injectFirst>
-      <SessionProvider>
-        <CssBaseline />
-        {children}
-      </SessionProvider>
-    </StyledEngineProvider>
-  );
+  return <SessionProvider>{children}</SessionProvider>;
 };
 
 export default Provider;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   important: '#app',
-  corePlugins: {
-    preflight: false,
-  },
+  corePlugins: {},
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
-      'white': '#FFFFFF',
-      'blue': '#5DE5FF',
+      white: '#FFFFFF',
+      blue: '#5DE5FF',
       'midnight-blue': '#003C5A',
-      'yellow': '#FFDE4E',
-      'pink': '#FF6397',
+      yellow: '#FFDE4E',
+      pink: '#FF6397',
       'coral-pink': '#FF6D9D',
-      'orange': '#FFA957',
-      'turquoise': '#4BEFBD',
-      'purple': '#CE8FFF',
+      orange: '#FFA957',
+      turquoise: '#4BEFBD',
+      purple: '#CE8FFF',
       'dark-blue': '#172E59',
       'teal-blue': '#00678C',
-      'black': '#1D1D1D'
+      black: '#1D1D1D',
     },
   },
   plugins: [require('@tailwindcss/forms')],
