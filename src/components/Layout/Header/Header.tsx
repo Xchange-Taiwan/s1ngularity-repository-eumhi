9268fa19e763ce0ebd39f1a3a6caacba80548bf7
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-09-23 15:29:16 +0800

    refactor: Adjust Header html stucture

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { FC } from 'react';
 
 import logoImgUrl from './assets/logo.svg';
@@ -15,30 +16,35 @@ declare global {
 
 export const Header: FC = () => {
   return (
-    <header className="fixed z-50 flex h-[70px] w-full items-center justify-between bg-white px-11 text-center text-2xl">
-      <div>
+    <header className="fixed inset-x-0 z-50 bg-white px-11 text-2xl">
+      <div className="flex h-[70px] items-center justify-between">
         <Image src={logoImgUrl} alt="logo" />
-      </div>
-      <div>
-        <a
-          href="/"
-          className="mr-7 hidden font-['Open_Sans'] text-base text-black md:inline"
-        >
-          首頁
-        </a>
-        <a
-          href="about"
-          className="mr-7 hidden font-['Open_Sans'] text-base text-black md:inline"
-        >
-          關於 X-Talent
-        </a>
-        <button className="mr-7 hidden h-10 w-20 rounded-md border-2 border-solid border-teal-blue bg-white text-base font-bold text-teal-blue md:inline">
-          註冊
-        </button>
-        <button className="hidden h-10 w-20 rounded-md bg-teal-blue text-base font-bold text-white md:inline">
-          登入
-        </button>
-        <div className="flex-none md:hidden">
+
+        <div className="hidden gap-7 md:flex md:items-center">
+          <Link href="/" className="font-['Open_Sans'] text-base text-black">
+            首頁
+          </Link>
+          <Link
+            href="/about"
+            className="font-['Open_Sans'] text-base text-black"
+          >
+            關於 X-Talent
+          </Link>
+          <Link
+            className="box-border h-10 rounded-md border-2 border-solid border-teal-blue bg-white px-6 py-2 text-base font-bold text-teal-blue"
+            href="/signup"
+          >
+            註冊
+          </Link>
+          <Link
+            className="h-10 rounded-md bg-teal-blue px-6 py-2 text-base font-bold  text-white"
+            href="/login"
+          >
+            登入
+          </Link>
+        </div>
+
+        <div className="md:hidden">
           <button
             onClick={() => window.modal.showModal()}
             className="btn btn-square btn-ghost"
@@ -58,50 +64,60 @@ export const Header: FC = () => {
             </svg>
           </button>
         </div>
-        <dialog id="modal" className="modal modal-bottom sm:modal-middle">
-          <form
-            method="dialog"
-            className="modal-box flex	h-full max-h-full flex-col items-center justify-center rounded-none"
-          >
-            <a
-              href="/"
-              className="font-['Open_Sans'] text-xl text-black md:hidden"
-            >
+      </div>
+
+      <dialog id="modal" className="modal modal-bottom sm:modal-middle">
+        <form
+          method="dialog"
+          className="modal-box flex	h-full max-h-full flex-col items-center justify-center rounded-none"
+        >
+          <div className="modal-action">
+            <button className="btn btn-circle absolute right-5 top-5">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+
+          <div className="mb-24 flex flex-col gap-12 text-center">
+            <Link href="/" className="font-['Open_Sans'] text-xl text-black">
               首頁
-            </a>
-            <a
-              href="about"
-              className="my-[30px] font-['Open_Sans'] text-xl  text-black md:hidden"
+            </Link>
+            <Link
+              href="/about"
+              className="font-['Open_Sans'] text-xl  text-black"
             >
               關於 X-Talent
-            </a>
-            <button className="my-10 h-10 w-40 rounded-md bg-teal-blue text-base font-bold text-white md:hidden">
+            </Link>
+          </div>
+
+          <div className="flex w-40 flex-col gap-8">
+            <Link
+              className="rounded-md bg-teal-blue px-6 py-2 text-center text-base font-bold text-white"
+              href="/login"
+            >
               登入
-            </button>
-            <button className="h-10 w-40 rounded-md border-2 border-solid border-teal-blue bg-white text-base font-bold text-teal-blue md:hidden">
+            </Link>
+            <Link
+              className="box-border h-10 rounded-md border-2 border-solid border-teal-blue bg-white px-6 py-2 text-center text-base font-bold  text-teal-blue"
+              href="/signup"
+            >
               註冊
-            </button>
-            <div className="modal-action">
-              <button className="btn btn-circle absolute right-5 top-5">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-          </form>
-        </dialog>
-      </div>
+            </Link>
+          </div>
+        </form>
+      </dialog>
     </header>
   );
 };
