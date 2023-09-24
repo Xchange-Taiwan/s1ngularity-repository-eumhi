4676631dc47111190fee45f103e2e6c97f81252e
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-09-24 22:30:51 +0800

    fix: remove unnecessary change

--- src/types/react-app-env.d.ts
+++ src/types/react-app-env.d.ts
@@ -2,14 +2,4 @@
 declare module '*.png';
 declare module '*.jpeg';
 declare module '*.jpg';
-
-declare module '*.svg' {
-  import * as React from 'react';
-
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-
-  const src: string;
-  export default src;
-}
+declare module '*.svg';
