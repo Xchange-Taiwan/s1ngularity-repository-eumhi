5f189e799a9dbffa71d6f0856dc43f866fd3a0df
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-10-07 20:03:38 +0800

    feat: Use react hook form to handle sign-in

--- src/app/(contentLayout)/(auth)/auth/signin/page.tsx
+++ src/app/(contentLayout)/(auth)/auth/signin/page.tsx
@@ -1,16 +1,49 @@
 'use client';
+import { zodResolver } from '@hookform/resolvers/zod';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 
 import LogoImgUrl from '@/assets/logo.svg';
 import { Google } from '@/components/Icon';
 import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { cn } from '@/lib/utils';
 
-const linkStyle = 'text-sm font-medium text-black underline underline-offset-2';
+const formSchema = z.object({
+  email: z.string().email('請輸入電子郵件'),
+  password: z.string().min(8, { message: '密碼至少需為 8 個字' }),
+});
+
+const linkStyle =
+  'text-sm font-medium text-black underline underline-offset-2 cursor-pointer';
 
 export default function Page() {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <div className="mb-10">
@@ -22,21 +55,51 @@ export default function Page() {
         </h1>
 
         <div className="flex flex-col gap-4">
-          <div className="grid max-w-sm items-center gap-1.5">
-            <Label htmlFor="email">電子郵件地址</Label>
-            <Input id="email" type="email" placeholder="請填入您的 E-mail" />
-          </div>
+          <Form {...form}>
+            <form className="grid gap-4" onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel showErrorStyle={false}>電子郵件地址</FormLabel>
+                    <FormControl>
+                      <Input placeholder="請填入您的 E-mail" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-          <div className="grid max-w-sm items-center gap-1.5">
-            <Label htmlFor="password">登入密碼</Label>
-            <Input id="password" type="password" placeholder="請輸入密碼" />
-          </div>
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel showErrorStyle={false}>登入密碼</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="請輸入密碼"
+                        type="password"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                    <FormDescription
+                      className={cn(linkStyle, 'inline-block')}
+                      onClick={() => router.push('/auth/passwordForget')}
+                    >
+                      忘記密碼
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
 
-          <Link className={linkStyle} href="/auth/passwordForget">
-            忘記密碼
-          </Link>
-
-          <Button className="h-12 w-full rounded-full">登入</Button>
+              <Button className="h-12 w-full rounded-full" type="submit">
+                登入
+              </Button>
+            </form>
+          </Form>
 
           <p className="text-neutral-600">
             還不是會員?{' '}
