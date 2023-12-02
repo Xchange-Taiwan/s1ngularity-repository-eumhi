357c269a4b7cca5fb499119511634b502052baf3
ChengYiLin <cheng.yi.lin.217@gmail.com>
2023-12-02 18:49:44 +0800

    style: fixup the linter error

--- src/components/ui/selectOptions.tsx
+++ src/components/ui/selectOptions.tsx
@@ -1,4 +1,8 @@
-import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
+import {
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+} from '@radix-ui/react-icons';
 import * as Select from '@radix-ui/react-select';
 import * as React from 'react';
 
@@ -25,19 +29,19 @@ const SelectItem = React.forwardRef<HTMLDivElement, SelectItemProps>(
     return (
       <Select.Item
         className={cn(
-          'text-[13px] leading-none text-black rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-primary data-[highlighted]:text-white',
-          className
+          'data-[disabled]:text-mauve8 relative flex h-[25px] select-none items-center rounded-[3px] pl-[25px] pr-[35px] text-[13px] leading-none text-black data-[disabled]:pointer-events-none data-[highlighted]:bg-primary data-[highlighted]:text-white data-[highlighted]:outline-none',
+          className,
         )}
         {...props}
         ref={ref}
       >
         <Select.ItemText>{children}</Select.ItemText>
-        <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
+        <Select.ItemIndicator className="absolute left-0 inline-flex w-[25px] items-center justify-center">
           <CheckIcon />
         </Select.ItemIndicator>
       </Select.Item>
     );
-  }
+  },
 );
 SelectItem.displayName = 'SelectItem';
 
@@ -46,17 +50,17 @@ const SelectOptions = React.forwardRef<HTMLDivElement, SelectOptionsProps>(
     return (
       <Select.Root>
         <Select.Trigger
-          className="inline-flex border border-input items-center rounded px-3 py-2 text-sm leading-none h-10 gap-[5px] text-violet11 outline-none data-[placeholder]:text-muted-foreground"
+          className="text-violet11 inline-flex h-10 items-center gap-[5px] rounded border border-input px-3 py-2 text-sm leading-none outline-none data-[placeholder]:text-muted-foreground"
           aria-label="Food"
         >
           <Select.Value placeholder={selectItemData.placeholder} />
-          <Select.Icon className="ml-auto text-violet11">
+          <Select.Icon className="text-violet11 ml-auto">
             <ChevronDownIcon />
           </Select.Icon>
         </Select.Trigger>
         <Select.Portal>
-          <Select.Content className="z-[60] overflow-hidden bg-white rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
-            <Select.ScrollUpButton className="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
+          <Select.Content className="z-[60] overflow-hidden rounded-md bg-white shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
+            <Select.ScrollUpButton className="text-violet11 flex h-[25px] cursor-default items-center justify-center bg-white">
               <ChevronUpIcon />
             </Select.ScrollUpButton>
             <Select.Viewport className="p-[5px]">
@@ -65,20 +69,23 @@ const SelectOptions = React.forwardRef<HTMLDivElement, SelectOptionsProps>(
                   {selectItemData.label}
                 </Select.Label>
                 {selectItemData.options.map((option) => {
-                  return <SelectItem key={option} value={option}>{option}</SelectItem>;
+                  return (
+                    <SelectItem key={option} value={option}>
+                      {option}
+                    </SelectItem>
+                  );
                 })}
               </Select.Group>
             </Select.Viewport>
-            <Select.ScrollDownButton className="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
+            <Select.ScrollDownButton className="text-violet11 flex h-[25px] cursor-default items-center justify-center bg-white">
               <ChevronDownIcon />
             </Select.ScrollDownButton>
           </Select.Content>
         </Select.Portal>
       </Select.Root>
     );
-  }
-)
+  },
+);
 SelectOptions.displayName = 'SelectOptions';
 
 export { SelectOptions };
-
