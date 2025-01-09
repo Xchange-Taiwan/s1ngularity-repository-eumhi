5ae907c6be663918f25becd2d5721ab752238a7d
Jack Lee <jack20499@gmail.com>
2025-01-09 08:34:10 -0800

    feat: display failure page for expired token on email verification

--- src/app/auth/emailVerified/ui.tsx
+++ src/app/auth/emailVerified/ui.tsx
@@ -1,13 +1,20 @@
 import Image from 'next/image';
 
-import EmailVerifiedIconUrl from '@/assets/auth/email-verified-icon.svg';
 import { Button } from '@/components/ui/button';
 
 interface EmailVerifiedPresentationProps {
+  icon: string;
+  title: string;
+  content: string;
+  btnContent: string;
   onSetProfile: () => void;
 }
 
 export default function EmailVerifiedPresentation({
+  icon,
+  title,
+  content,
+  btnContent,
   onSetProfile,
 }: EmailVerifiedPresentationProps) {
   return (
@@ -15,21 +22,19 @@ export default function EmailVerifiedPresentation({
       <div className="relative h-[108px] bg-[#EBFBFB]">
         <Image
           className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-2/3 transform"
-          src={EmailVerifiedIconUrl.src}
+          src={icon}
           alt="Verify Email"
           width={80}
           height={80}
         />
       </div>
       <div className="flex flex-col items-center gap-6 p-20 text-center">
-        <h1 className="text-[32px] font-bold leading-10">驗證成功</h1>
+        <h1 className="text-[32px] font-bold leading-10">{title}</h1>
 
-        <p className="text-neutral-600">
-          你的帳號已完成註冊。現在可以開始建立你的個人頁面和尋找 Mentors 了！
-        </p>
+        <p className="text-neutral-600">{content}</p>
 
         <Button className="max-w-60 rounded-full" onClick={onSetProfile}>
-          設定個人資訊
+          {btnContent}
         </Button>
       </div>
     </div>
