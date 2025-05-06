bc7c298151f03ac9832ae52b14a08022f9611299
LowSugarCoke <jack20499@gmail.com>
2025-05-05 21:45:50 -0400

    update fields in edit page (#197)
    ## What Does This PR Do?
    - Update fields in edit page
    
    ## Demo
    
    http://localhost:3000/profile/123/edit
    
    ## Screenshot
    
    ![image](https://github.com/user-attachments/assets/60123457-fe5c-4220-a084-16826f36b3f8)
    
    
    ## Anything to Note?
    
    N/A

--- src/app/profile/[userId]/edit/page.tsx
+++ src/app/profile/[userId]/edit/page.tsx
@@ -21,12 +21,19 @@ import {
 import { Section } from '@/components/profile/edit/section';
 import { Button } from '@/components/ui/button';
 import { Form } from '@/components/ui/form';
+import useLocations from '@/hooks/user/country/useLocations';
+import useIndustries from '@/hooks/user/industry/useIndustries';
 
 const onSubmit = (values: z.infer<typeof formSchema>) => {
   console.log(values);
 };
 
 export default function Page() {
+  const isMentor = true;
+
+  const { locations } = useLocations('zh_TW');
+  const { industries } = useIndustries('zh_TW');
+
   const form = useForm<ProfileFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues,
@@ -65,17 +72,32 @@ export default function Page() {
             name="avatarFile"
             avatarUrl={''}
           />
-
           <Section title="姓名">
             <TextField form={form} name="name" placeholder="請填入您的姓名" />
           </Section>
 
-          <Section title="Personal Statement">
-            <TextField form={form} name="statement" />
-          </Section>
+          {isMentor && (
+            <Section title="我能提供的服務">
+              <h1> TBD</h1>
+            </Section>
+          )}
 
-          <Section title="關於我">
-            <TextareaField form={form} name="about" rows={6} />
+          {isMentor && (
+            <Section title="專業能力">
+              <h1> TBD</h1>
+            </Section>
+          )}
+
+          <Section title="地區">
+            <SelectField
+              form={form}
+              name="region"
+              placeholder="請選擇地區"
+              options={locations.map((loc) => ({
+                value: loc.value,
+                label: loc.text,
+              }))}
+            />
           </Section>
 
           <Section title="職務級別">
@@ -85,6 +107,34 @@ export default function Page() {
               placeholder="請填入您的職務級別"
               options={[{ label: 'junior', value: 'junior' }]}
             />
+          </Section>
+
+          <Section title="產業">
+            <SelectField
+              form={form}
+              name="industry"
+              placeholder="請選擇產業"
+              options={industries.map((loc) => ({
+                value: loc.subject_group,
+                label: loc.subject,
+              }))}
+            />
+          </Section>
+
+          <Section title="關於我">
+            <TextareaField form={form} name="about" rows={6} />
+          </Section>
+
+          <Section title="有興趣多了解的職位">
+            <h1> TBD</h1>
+          </Section>
+
+          <Section title="想多了解、加強的技能">
+            <h1> TBD</h1>
+          </Section>
+
+          <Section title="想多了解的主題">
+            <h1> TBD</h1>
           </Section>
 
           <JobExperienceSection form={form} />
