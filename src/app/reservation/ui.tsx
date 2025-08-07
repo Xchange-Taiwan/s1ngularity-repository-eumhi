eb54abbb51da6a23d644ea4f690aa42d502f61fa
Jack Lee <jack20499@gmail.com>
2025-08-06 20:07:43 -0400

    feat: add title

--- src/app/reservation/ui.tsx
+++ src/app/reservation/ui.tsx
@@ -1,7 +1,15 @@
 export default function ReservationPresentation() {
   return (
-    <div className="mx-auto my-40 max-w-[90%] overflow-hidden rounded-2xl border-2 border-solid border-background-border md:max-w-[630px]">
-      <div>Hello World</div>
+    <div className="flex  min-h-screen justify-center ">
+      <div className="w-full max-w-[90%] overflow-hidden rounded-2xl md:max-w-[630px]">
+        <div className="font-roboto mx-auto mb-6 h-[42px] w-[251px] text-[36px] font-semibold leading-[100%] tracking-[0%] text-[#1D1B20]">
+          我的預約
+        </div>
+
+        <div className="flex w-full flex-col gap-6">
+          <div className="bg-white rounded p-4 shadow">Tab component</div>
+        </div>
+      </div>
     </div>
   );
 }
